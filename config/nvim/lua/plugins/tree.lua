b59d0d23d071d237afd77adf374b670eca71cefd
Colby Thomas <colby@jrnxf.co>
2022-02-20 14:46:04 -0700

    nvim tree updates

--- config/nvim/lua/plugins/tree.lua
+++ config/nvim/lua/plugins/tree.lua
@@ -1,34 +1,31 @@
 return function()
   local nvim_tree = require 'nvim-tree'
-  local map = require('nvim-tree.config').nvim_tree_callback
 
   nvim_tree.setup {
     auto_close = true,
-    disable_netrw = true,
     update_focused_file = {
       enable = true,
     },
     diagnostics = {
       enable = true,
     },
-    hijack_cursor = true,
     view = {
       auto_resize = true,
       mappings = {
-        list = {
-          { key = '<C-R>', cb = map 'refresh' },
-          { key = 'a', cb = map 'create' },
-          { key = 'd', cb = map 'remove' },
-          { key = 'h', cb = map 'close_node' },
-          { key = 'I', cb = map 'toggle_ignored' },
-          { key = 'l', cb = map 'edit' },
-          { key = 'r', cb = map 'rename' },
-          { key = 's', cb = map 'split' },
-          { key = 'v', cb = map 'vsplit' },
-          { key = 'Y', cb = map 'copy_path' },
-          { key = 'y', cb = map 'copy_name' },
-        },
         custom_only = false, -- false means the list above will merge with defaults
+        list = {
+          { key = '<C-R>', action = 'refresh' },
+          { key = 'a', action = 'create' },
+          { key = 'd', action = 'remove' },
+          { key = 'h', action = 'close_node' },
+          { key = 'I', action = 'toggle_ignored' },
+          { key = 'l', action = 'edit' },
+          { key = 'r', action = 'rename' },
+          { key = 's', action = 'split' },
+          { key = 'v', action = 'vsplit' },
+          { key = 'Y', action = 'copy_path' },
+          { key = 'y', action = 'copy_name' },
+        },
       },
     },
     actions = {
