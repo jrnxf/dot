58c00379aa95c63b4bd9734aa7d1e30cef9de787
Colby Thomas <colby@jrnxf.co>
2026-05-04 13:28:31 +0100

    wipe nvim config, archive old setup as nvim.bak
    Old plugin/packer-based config was broken against current nvim;
    preserved at config/nvim.bak for reference. install now nukes
    all nvim runtime dirs so machines start barebones.

--- setup/neovim.sh
+++ setup/neovim.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-# commenting out for now - broken and not interested in fixing rn
-# rm -rf ~/.cache/nvim ~/.config/nvim/plugin ~/.local/share/nvim
-# git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-# nvim --headless -u NONE -c 'lua require("jrnxf.lib.reload").full_reload()' -c 'PackerSync' -c 'au User MasonToolsUpdateCompleted quitall'
+# Wipe all Neovim state so each machine starts from a barebones install.
+# Old config is preserved in this repo at config/nvim.bak for reference.
+set -e
+
+rm -rf \
+  "${HOME}/.config/nvim" \
+  "${HOME}/.cache/nvim" \
+  "${HOME}/.local/share/nvim" \
+  "${HOME}/.local/state/nvim"
